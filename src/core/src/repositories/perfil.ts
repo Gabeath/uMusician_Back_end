fd522b34edc1b876ba5585a7cc58753507c24758
Gabriel <gabrielzambuzi7@gmail.com>
2021-04-20 22:13:39 -0300

    ajuste filtro

--- src/core/src/repositories/perfil.ts
+++ src/core/src/repositories/perfil.ts
@@ -29,56 +29,60 @@ export class RepositoryPerfil implements IRepositoryPerfil {
 
   async getMusicosWithSearchParameters(searchParameter: IPerfilSearchParameter):
   Promise<Pagination<EntidadePerfil>> {
-    let medias: { idPerfil: string, media: string }[] = await this.repositoryAvaliacao
+    let idsPerfis: string[] = [];
+    const medias: { idPerfil: string, media: string }[] = await this.repositoryAvaliacao
       .createQueryBuilder('avaliacao')
       .select('AVG(avaliacao.pontuacao)', 'media')
       .addSelect('avaliacao.idPerfil', 'idPerfil')
       .groupBy('avaliacao.idPerfil')
       .getRawMany();
 
-    if (searchParameter.pontuacaoAvaliacao) {
-      medias = medias
-        .filter((avaliacao) => parseFloat(avaliacao.media) >= searchParameter.pontuacaoAvaliacao);
-    }
-
-    const avaliacoes: EntidadeAvaliacao[] = medias.map((avaliacao) => {
+    let avaliacoes: EntidadeAvaliacao[] = medias.map((avaliacao) => {
       return {
         idPerfil: avaliacao.idPerfil,
         pontuacao: parseFloat(avaliacao.media),
       } as EntidadeAvaliacao;
     });
 
+    if (searchParameter.pontuacaoAvaliacao) {
+      avaliacoes = avaliacoes
+        .filter((avaliacao) => avaliacao.pontuacao >= searchParameter.pontuacaoAvaliacao);
+
+      const idsPerfisAvaliacoes = avaliacoes.map((avaliacao) => {
+        return avaliacao.idPerfil;
+      });
+
+      idsPerfis = idsPerfisAvaliacoes;
+    }
+
     const generosMusicais: EntidadeGeneroMusicalPerfil[] = await this.repositoryGeneroMusicalPerfil
       .find({
         where: {
+          ...(idsPerfis.length > 0 && { idPerfil: In(idsPerfis) }),
           ...(searchParameter.generoMusical && { id: searchParameter.generoMusical }),
-        }
+        },
       });
+
+    const idsPerfisGenerosMusicais = generosMusicais.map((generoMusical) => {
+      return generoMusical.idPerfil;
+    });
+
+    idsPerfis = idsPerfisGenerosMusicais;
 
     const apresentacoes: EntidadeApresentacao[] = await this.repositoryApresentacao.find({
       where: {
+        idPerfil: In(idsPerfis),
         ...(searchParameter.especialidade && { id: searchParameter.especialidade }),
         ...(searchParameter.valorMinimo && searchParameter.valorMaximo &&
           { valorHora: Between(searchParameter.valorMinimo, searchParameter.valorMaximo) }),
-      }
+      },
     });
 
-    const idsPerfis: string[] = [];
-    avaliacoes.forEach((avaliacao) => {
-      if (!idsPerfis.find((idPerfil) => idPerfil === avaliacao.idPerfil)) {
-        idsPerfis.push(avaliacao.idPerfil);
-      }
+    const idsPerfisApresentacoes = apresentacoes.map((apresentacao) => {
+      return apresentacao.idPerfil;
     });
-    generosMusicais.forEach((generoMusical) => {
-      if (!idsPerfis.find((idPerfil) => idPerfil === generoMusical.idPerfil)) {
-        idsPerfis.push(generoMusical.idPerfil);
-      }
-    });
-    apresentacoes.forEach((apresentacao) => {
-      if (!idsPerfis.find((idPerfil) => idPerfil === apresentacao.idPerfil)) {
-        idsPerfis.push(apresentacao.idPerfil);
-      }
-    });
+
+    idsPerfis = idsPerfisApresentacoes;
 
     const usuarios: EntidadeUsuario[] = await this.repositoryUsuario
       .createQueryBuilder('usuario')
@@ -100,7 +104,7 @@ export class RepositoryPerfil implements IRepositoryPerfil {
         categoria: CategoriaPerfil.MUSICO,
         ...(searchParameter.cidade && { cidade: searchParameter.cidade }),
         ...(searchParameter.estado && { estado: searchParameter.estado }),
-      }
+      },
     });
 
     perfis.forEach((perfil) => {
