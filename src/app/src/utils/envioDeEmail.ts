5cbed330d2a5cc86ecdda8b68f8d9b51ac7267d7
Angelo Scolfaro <angelo.cheri@venturus.org.br>
2021-08-13 16:50:32 -0300

    criando endpoints necessarios para o fluxo de recuperação de senha

--- src/app/src/utils/envioDeEmail.ts
+++ src/app/src/utils/envioDeEmail.ts
@@ -9,10 +9,12 @@ const sendResetPasswordMail = async (name: string, to: string, codigo: string) :
       a solicitação é ${codigo}. Não compartilhe! Se não foi você, ignore esta mensagem.`,
     html: `
           <h2> Olá, ${name} </h2>
-          <p> notamos que você solicitou a redefinição de senha. O código para prosseguir com 
-          a solicitação é ${codigo} </p>
+          <p> Notamos que você solicitou a redefinição de senha. O código para prosseguir com 
+          a solicitação é: </p>
+          <p> ${codigo} </p>
+          <p> Este código expira em 24 horas.</p>
           
-          <p>Não compartilhe! Se não foi você, <strong> ignore esta mensagem <strong>.</p>
+          <p><strong>Não compartilhe!</strong> Se não foi você, ignore esta mensagem.</p>
       `
   };
   await emailService.sendMail(email);
